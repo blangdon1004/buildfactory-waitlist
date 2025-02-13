fd791cf890babb6001d211158d43e68b17521db8
Shobhit.Gupta <24531736+shobhi088@users.noreply.github.com>
2025-02-12 21:24:14 -0800

    Reverting back changes as Privacy page itself is fine
    Reverting back changes as Privacy page itself is fine

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Privacy />} />
+          <Route path="/" element={<Index />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
