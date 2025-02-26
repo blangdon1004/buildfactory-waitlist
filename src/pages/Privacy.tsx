5d0bb9155e0d5a3b550cbbfa06de30750ec7e6eb
blangdon1004 <blangdon@buildfactory.io>
2025-02-26 11:53:15 -0600

    Update Privacy.tsx
    <!-- Google tag (gtag.js) -->
    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM0NLRJBDK"></script>
    <script>
      window.dataLayer = window.dataLayer || [];
      function gtag(){dataLayer.push(arguments);}
      gtag('js', new Date());
    
      gtag('config', 'G-LM0NLRJBDK');
    </script>

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -5,6 +5,15 @@ const Privacy = () => {
     <div className="min-h-screen bg-background">
       {/* Header */}
       <header className="fixed top-0 left-0 right-0 h-16 bg-white shadow-sm z-50">
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LM0NLRJBDK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LM0NLRJBDK');
+</script>
         <div className="container h-full flex items-center justify-between">
           <a href="/">
             <img 
