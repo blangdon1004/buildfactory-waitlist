4236df80a9938cad20db0847d10673ece0c5ab48
blangdon1004 <blangdon@buildfactory.io>
2025-02-27 12:57:18 -0600

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM0NLRJBDK"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-LM0NLRJBDK');
