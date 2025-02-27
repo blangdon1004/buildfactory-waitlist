9469e35750ac2e7543862603b30ae168992f38f0
blangdon1004 <blangdon@buildfactory.io>
2025-02-27 12:58:45 -0600

    Update Terms.tsx

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -9,7 +9,7 @@ const Terms = () => {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-LM0NLRJBDK"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-LM0NLRJBDK');
